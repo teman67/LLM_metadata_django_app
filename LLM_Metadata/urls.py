cbd685c30f1051a3b1fc12d0eda7c10f61604409
Amirhossein Bayani <48212448+teman67@users.noreply.github.com>
2025-07-28 13:50:45 +0200

    Update urls.py and add health check url

--- LLM_Metadata/urls.py
+++ LLM_Metadata/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('ask/', views.ask_question_view, name='ask_question'),
     path('json-viewer/', views.json_viewer, name='json_viewer'),
         path('delete_conversation/<int:user_convo_id>/', views.delete_conversation, name='delete_conversation'),
+    path('health/', views.health_check, name='health_check'),
 ]
