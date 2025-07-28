98bede1631767a45a1fd7bb94d0dc36c62d81767
Amirhossein Bayani <amirhossein.bayani@gmail.com>
2025-07-28 14:07:29 +0200

    set debug to false in setting

--- main/settings.py
+++ main/settings.py
@@ -29,7 +29,8 @@ print(BASE_DIR)
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+# DEBUG = os.getenv('DEBUG')
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1' , '.herokuapp.com']
 
